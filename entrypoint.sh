8b69e998ae5433e2468ec8ecc51bce1e16c4a011
Digitroinc <craigbett@gmail.com>
2017-07-07 09:59:49 -0400

    //wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ sleep 2
 if [[ ! -d /home/container/server ]] || [[ ${UPDATE} == "1" ]]; then
 
 	if [[ -f /home/container/steam.txt ]]; then
-		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container/server +runscript /home/container/steam.txt
+		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container/server +app_update ${APP_ID} validate +runscript /home/container/steam.txt
 	else
 		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container/server +app_update ${APP_ID} validate +quit
 	fi
@@ -24,6 +24,20 @@ if [[ ! -d /home/container/server ]] || [[ ${UPDATE} == "1" ]]; then
 	#fix
 	if [[ ! -d /home/container/.local/share ]]; then
 		mkdir -p /home/container/.local/share
+	fi
+	
+	#workshop
+	if [[ ! -d /home/container/server/steamapps/workshop ]]; then 
+		mkdir -p /home/container/server/steamapps/workshop
+	fi
+	
+	if [[ ! -d /home/container/server/steamapps/workshop ]]; then 
+		mkdir -p /home/container/server/steamapps/workshop
+	fi
+	
+	if [[ ! -L home/container/server/workshop ]]; then 
+		rm home/container/server/workshop
+		ln -s /home/container/server/steamapps/workshop home/container/server/workshop
 	fi
 	
 fi
