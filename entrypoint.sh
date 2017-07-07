2d455f4fbca3f73d5ec9ca58825dfe9a132d7c14
Digitroinc <craigbett@gmail.com>
2017-07-07 10:04:32 -0400

    //wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,43 +3,15 @@ sleep 2
 
 #Install the Server
 if [[ ! -d /home/container/server ]] || [[ ${UPDATE} == "1" ]]; then
-
 	if [[ -f /home/container/steam.txt ]]; then
 		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container/server +app_update ${APP_ID} validate +runscript /home/container/steam.txt
 	else
 		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container/server +app_update ${APP_ID} validate +quit
 	fi
+fi
 
-	#fix
-	if [[ -d /home/container/server/mpmissions ]]; then
-		ln -s /home/container/server/mpmissions /home/container/server/MPMissions
-	fi
-	
-	#fix
-	if [[ -f /home/container/server/steamclient.so ]]; then
-		rm /home/container/server/steamclient.so
-		ln -s /home/container/server/steamcmd/linux32/steamclient.so /home/container/server/steamclient.so
-	fi
-	
-	#fix
-	if [[ ! -d /home/container/.local/share ]]; then
-		mkdir -p /home/container/.local/share
-	fi
-	
-	#workshop
-	if [[ ! -d /home/container/server/steamapps/workshop ]]; then 
-		mkdir -p /home/container/server/steamapps/workshop
-	fi
-	
-	if [[ ! -d /home/container/server/steamapps/workshop ]]; then 
-		mkdir -p /home/container/server/steamapps/workshop
-	fi
-	
-	if [[ ! -L home/container/server/workshop ]]; then 
-		rm home/container/server/workshop
-		ln -s /home/container/server/steamapps/workshop home/container/server/workshop
-	fi
-	
+if [[ -f /home/container/preflight.sh ]]; then
+	/home/container/preflight.sh
 fi
 
 # Replace Startup Variables
