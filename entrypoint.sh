2deb68c596e3153aa6976e920e9bee4f852e91b6
xcpep <sudoplays@gmail.com>
2017-07-11 15:11:13 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,8 +23,6 @@ cd /home/container/server
 # $NSS_WRAPPER_PASSWD and $NSS_WRAPPER_GROUP have been set by the Dockerfile
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
-envsubst < /passwd.template > ${NSS_WRAPPER_PASSWD}
-export LD_PRELOAD=/libnss_wrapper.so
 
 # Run the Server
 ${MODIFIED_STARTUP}
