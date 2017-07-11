570ec46028f44f514d6b8fe50537d64da0bb05b9
xcpep <sudoplays@gmail.com>
2017-07-11 18:25:43 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 sleep 2
 
+mkdir -p /home/container/steamcmd
+cd /home/container/steamcmd
+wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+tar -zxvf steamcmd_linux.tar.gz
+rm -f steamcmd_linux.tar.gz
+chmod 777 *
+cd ~
+
 #Install the Server
 if [[ ! -d /home/container/server ]] || [[ ${UPDATE} == "1" ]]; then
 	if [[ -f /home/container/steam.txt ]]; then
-		mkdir -p /home/container/steamcmd
-		cd /home/container/steamcmd
-		wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
-		tar -zxvf steamcmd_linux.tar.gz
-		rm -f steamcmd_linux.tar.gz
-		chmod 777 *
 		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container/server +app_update ${APP_ID} validate +runscript /home/container/steam.txt
 	else
-		mkdir -p /home/container/steamcmd
-		cd /home/container/steamcmd
-		wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
-		tar -zxvf steamcmd_linux.tar.gz
-		rm -f steamcmd_linux.tar.gz
-		chmod 777 *
 		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container/server +app_update ${APP_ID} validate +quit
 	fi
 fi
